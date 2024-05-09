029f3c1fc6b4574a911a9341c36472a382040fa7
sssajjad007 <sssajjad.76@gmail.com>
2024-05-09 10:27:28 +0330

    feat: add textStyle as prop

--- src/PickerItem.tsx
+++ src/PickerItem.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { ColorValue, Text } from 'react-native';
+import { ColorValue, Text, TextStyle } from 'react-native';
 
 export interface PickerItemProps {
   color?: ColorValue;
   label: string;
   value: string | number;
   testID?: string;
+  textStyle?: TextStyle;
 }
 
 export const PickerItem = ({ color, label, ...props }: PickerItemProps) => {
